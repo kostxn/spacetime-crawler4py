7397994e348832c204e3a3b95c4729427b532187
kostxn <156470250+kostxn@users.noreply.github.com>
2024-04-23 18:12:04 -0700

    you are awesome!

--- crawler/worker.py
+++ crawler/worker.py
@@ -30,5 +30,6 @@ class Worker(Thread):
             scraped_urls = scraper.scraper(tbd_url, resp)
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
+                print(f'added {scraped_url}')
             self.frontier.mark_url_complete(tbd_url)
             time.sleep(self.config.time_delay)
